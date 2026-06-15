4956eff6dc5a2b83d51c574575254aadb815a072
Roberto Carlos Pulgar Roco <ryasud@gmail.com>
2026-06-14 20:34:11 -0400

    correciones

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import hero from './assets/hero.png'
+import logo from './assets/logo.jpg'
 import './App.css'
+
 import Card from './components/Cardcomponent/Card.jsx';
 import Image from './components/ImageComponent/Image.jsx';
 
@@ -15,12 +14,12 @@ function App() {
         <nav className="navbar navbar-expand-lg bg-white border-bottom shadow-sm sticky-top">
           <div className="container">
             <a className="navbar-brand" href="#">
-              <img src={hero} alt="Grupo G" height="120" />
+              <img src={logo} alt="Grupo G" height="120" />
             </a>
-            <button className="navbar-toggler" type="button" aria-expanded="false" aria-label="Toggle navigation">
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#menuPrincipal" aria-controls="menuPrincipal" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
-            <div className="navbar-menu" id="menuPrincipal">
+            <div className="navbar-collapse collapse" id="menuPrincipal">
               <ul className="navbar-nav ms-auto align-items-lg-center gap-lg-2">
                 <li className="nav-item"><a className="nav-link active" href="#">Inicio</a></li>
                 <li className="nav-item"><a className="nav-link" href="#caracteristicas">Características</a></li>
@@ -34,21 +33,13 @@ function App() {
 
       <section id="center">
         <div className="hero">
-          <img src={hero} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
+          <img src={logo} className="base" width="170" height="179" alt="logo" />
         </div>
         <div>
           <h1>SUPERMERCADO EL ECONOMICO</h1>
           <p>TIENDA DE ALIMENTOS</p>
         </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
+        
       </section>
 
       <div>
@@ -61,4 +52,4 @@ function App() {
   )
 }
 
-export default App
+export default App 
--- src/components/button/button.jsx
+++
@@ -1,8 +0,0 @@
-import "./button.css";
-
-function Button(propiedades) {
-    return (<button type="button" className="counter">
-        {propiedades.titulo}
-    </button>)
-}
-export default Button;
